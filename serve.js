405abf3f28d6e3263532123e0f9b451493e71392
Filippo Tortomasi <filippo.tortomasi@gmail.com>
2020-01-06 16:26:22 +0100

    Fix Allow access origin issue

--- serve.js
+++ serve.js
@@ -3,13 +3,23 @@ const express = require('express');
 const app = express();
 const googleshoppinglist = require('./index');
 const to = require('./lib/to');
-const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 require('dotenv').config()
 const creds = {
 	email: process.env.GOOGLEEMAIL,
 	password: process.env.GOOGLEPASSWORD
 };
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', async function (req, res) {
 	let [err, list] = await to(googleshoppinglist.getList(creds, { cookies: true }));
